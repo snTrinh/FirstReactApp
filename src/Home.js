6ff0a8e15d1c48cc58bfb751c051ce77bc54ebb0
Susan Trinh <snt.trinh@gmail.com>
2021-03-05 13:45:58 -0700

    Looking at creating custom events/functions with event arguments.

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,19 @@
 const Home = () => {
+
+    const handleClick = (e) =>{
+        console.log('hello fren', e);
+    }
+    // creating a function that takes in a parameter
+    const handleClickAgain = (name, e) =>{
+        console.log('hello ' + name, e.target);
+    }
+
     return ( 
         <div className="Home">
         <h2>Homepage</h2>
+        <button onClick={handleClick}>Click Me</button>
+        {/* wrap the following code within an anonymous function */}
+        <button onClick={(e) => handleClickAgain('Susan', e)}>Click Me Again</button>
         </div>
      );
 }
